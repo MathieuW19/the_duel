caf17f39f37e29bf3f6f571a7ea748d9929ba486
AurelienKarmann <aurelienkarmann99@gmail.com>
2021-11-09 13:14:37 +0100

    Ajout /tasks

--- crud/api/routes/users.js
+++ crud/api/routes/users.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.status(200).json({});
+router.get('/tasks', (req, res, next) => {
+  res.status(200).json({message : "Test"});
 });
 
 module.exports = router;
